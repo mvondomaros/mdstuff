e55251c1bcbc1b302f265997e82f872f08cb19c3
Michael von Domaros <mvondomaros@gmail.com>
2020-01-17 15:51:31 -0800

    - now importing the structure subpackage

--- mdstuff/__init__.py
+++ mdstuff/__init__.py
@@ -1,4 +1,5 @@
-from .core import NAMDUniverse, MDStuffError
 import mdstuff.dynamics
+import mdstuff.structure
+from .core import NAMDUniverse, MDStuffError
 
-__all__ = ["NAMDUniverse", "MDStuffError", "dynamics"]
+__all__ = ["NAMDUniverse", "MDStuffError", "dynamics", "structure"]
