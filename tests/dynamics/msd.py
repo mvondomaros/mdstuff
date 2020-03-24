aba0e31eb459a8bbe39ae6a400a267c3849d64f6
Michael von Domaros <mvondomaros@gmail.com>
2020-03-24 10:23:37 -0700

    - added msd test

---
+++ tests/dynamics/msd.py
@@ -0,0 +1,29 @@
+import glob
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mdstuff import NAMDUniverse
+from mdstuff.dynamics import SingleParticleMSD
+
+psf = os.path.abspath("../data/OzoneSqualeneBulk/structure.psf")
+dcds = [
+    os.path.abspath(p) for p in sorted(glob.glob("../data/OzoneSqualeneBulk/*.dcd"))
+]
+universe = NAMDUniverse(psf, dcds)
+
+ag = universe.select_atoms("not resname SQU")
+analysis = SingleParticleMSD(ag)
+universe.add_analysis(analysis)
+
+universe.run_analyses()
+
+plt.figure()
+msd, time = analysis.get()
+plt.plot(time, msd)
+
+plt.show()
+
+
+
