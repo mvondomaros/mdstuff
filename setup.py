3f9939b710dfd04dc1b4265a6556dbf3fce2cb07
Michael von Domaros <mvondomaros@gmail.com>
2020-06-05 12:54:27 -0700

    cleanup

--- setup.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from Cython.Build import cythonize
-from distutils.core import setup
-
-setup(
-    ext_modules=cythonize("mdstuff_old/structure/distances.pyx", language_level=3,),
-    include_dirs=[np.get_include()],
-    requires=["Cython", "numpy", 'MDAnalysis', 'tqdm', 'matplotlib'],
-)
